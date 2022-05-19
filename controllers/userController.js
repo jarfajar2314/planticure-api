b86dbf780d6f565d2e25e364c364d35d48d6caae
jarfajar2314 <mfajaryusuf@upi.edu>
2022-05-20 01:45:33 +0700

    add check email

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,12 +6,32 @@ const firestore = firebase.firestore();
 
 // Add User
 const addUser = async (req, res, next) => {
+    const data = req.body;
+    let userCol = firestore.collection('users');
+    
+    // Check email
+    let emailExist = false;
+    let query = await userCol.where('email', '==', data.email).limit(1).get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            // console.log(doc.id, '=>', doc.data());
+            emailExist = true;
+        });
+    })
+    .catch(err => {
+        console.log('Error getting documents', err);
+    });
+    // console.log(emailExist);
+    if(emailExist) {
+        return res.status(400).send('Email already registered');
+    }
+
+    // Insert
     try {
-        const data = req.body;
-        await firestore.collection('users').doc().set(data);
-        res.send('User register success');
+        await userCol.doc().set(data);
+        return res.send('User register success');
     } catch (error) {
-        res.status(400).send(error.message);
+        return res.status(400).send(error.message);
     }
 }
 
