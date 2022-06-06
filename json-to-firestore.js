98737ed5123087beca2f73e013f1f918be0891a4
jarfajar2314 <mfajaryusuf@upi.edu>
2022-06-06 21:05:10 +0700

    change user-routes, add login, and getDisease

--- json-to-firestore.js
+++ json-to-firestore.js
@@ -5,16 +5,31 @@ const plantJSON = require('./models/plant.json');
 let plants = plantJSON.plants;
 
 
-plants.forEach(function(obj) {
-    db.collection("plants").add({
-        name: obj.name,
-        image: obj.image,
-        disease: obj.disease,
-        description: obj.description,
-    }).then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
+// plants.forEach(function(obj) {
+//     db.collection("plants").add({
+//         name: obj.name,
+//         image: obj.image,
+//         disease: obj.disease,
+//         description: obj.description,
+//     }).then(function(docRef) {
+//         console.log("Document written with ID: ", docRef.id);
+//     })
+//     .catch(function(error) {
+//         console.error("Error adding document: ", error);
+//     });
+// });
+
+const ORDER_ITEMS = db.collection('plants')
+plants.forEach(function(obj){
+    ORDER_ITEMS.where('name', '==', obj.name)
+      .get()
+      .then(snapshots => {
+        if (snapshots.size > 0) {
+          snapshots.forEach(orderItem => {
+            ORDER_ITEMS.doc(orderItem.id).update({ image: obj.image, })
+            console.log(`${obj.name} updated`);
+          })
+        }
+      });
 });
+
